44a82f3156d7a1a7de02c63ddd601add4b983cb3
Gillysuit <contemporarygil@gmail.com>
2019-07-30 12:33:46 -0700

    one more time

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import { Users } from './components/Users';
-import Team  from './components/Team';
 
 
 function App() {
   return (
     <div className="App">
-      <Team></Team>
       <Users></Users>
     </div>
   );
