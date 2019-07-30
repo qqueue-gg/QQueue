80d0f21d11c2c7c982e88ac188f11165a8757006
Gillysuit <contemporarygil@gmail.com>
2019-07-30 10:52:10 -0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Teams from './components/Teams';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,6 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Teams></Teams>
     </div>
   );
 }
---
+++ src/components/Teams.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Team = ({TeamName, TeamData}) => (
+  <div>
+    <h2>Yo</h2>
+  </div>
+);
+
+// Team.PropTypes = {
+//   TeamName: PropTypes.string.isRequired,
+//   TeamData: PropTypes.object.isRequired
+// }
+
+export default Team;
