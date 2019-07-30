9d91b2ef95c3ff7440cb75cdc906cf6367886c1c
Gillysuit <contemporarygil@gmail.com>
2019-07-30 15:49:22 -0700

    some endpoints

--- server/server.js
+++ server/server.js
@@ -4,8 +4,33 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send("i'm gilbert")
-})
+});
+
+/*   User Endpoints     */
+app.get('/getUsers', (req, res) =>{
+  res.status(200).send('ye');
+});
+
+app.put('/updateUser', (req, res) => {
+  res.status(200).send('work');
+});
+
+app.delete('/deleteUser', (requ, res) => {
+  res.status(200).send('it');
+});
+
+
+
+/*    Team EndPoints   */
+app.get('/getTeams', (req, res) =>{
+  res.status(200).send('okay');
+});
+app.put('/updateTeam', (req, res) => {
+  res.status(200).send('you');
+});
+
+app.delete('/deleteTeam', (requ, res) => {
+  res.status(200).send('chill');
+});
 
 app.listen(8080, () => 'gilbert is always watching');
-
-
