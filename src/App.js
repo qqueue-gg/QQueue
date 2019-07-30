9f773710ba6107bc3328db2b8119ea988459986f
Gillysuit <contemporarygil@gmail.com>
2019-07-30 12:17:40 -0700

    removed redundency

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import { Users } from './components/Users';
-import Team from './components/Team';
-import Users from './components/Users';
+import Team  from './components/Team';
+
 
 function App() {
   return (
