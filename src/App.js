42382093513481aa62a158f21ae5c55129a15775
Gillysuit <contemporarygil@gmail.com>
2019-07-30 15:22:06 -0700

    added rendering for both

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Users } from './components/Users';
+import { Teams } from './components/Teams';
 
 
 function App() {
   return (
     <div className="App">
       <Users></Users>
+      <Teams></Teams>
     </div>
   );
 }
