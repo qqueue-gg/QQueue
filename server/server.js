90f3a158ccffa06dec54837f727fda716a791c76
Gillysuit <contemporarygil@gmail.com>
2019-07-30 16:26:13 -0700

    adding stuff

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 
-<<<<<<< HEAD
 const userRoutes = require('./routes/userRoutes');
 const teamRoutes = require('./routes/userRoutes');
 
@@ -9,18 +8,9 @@ app.get('/', (req, res) => {
   res.send("i'm gilbert")
 });
 
-
-
-/*   User Endpoints     */
+/*   Routing Endpoints     */
 app.use('/user', userRoutes);
 app.use('/team', teamRoutes);
-
-
-
-/*    Team EndPoints   */
-=======
-app.listen(8080, () => 'gilbert is always watching');
->>>>>>> e3f32f7f3aab57f5a51d584971ed824d15a17323
 
 
 app.listen(8080, () => 'gilbert is always watching');
