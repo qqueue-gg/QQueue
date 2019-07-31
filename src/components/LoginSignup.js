714e8e0a13cdd496c454932709807d284c5a4ad5
Joseph Eisele <eisele.joseph1@gmail.com>
2019-07-30 19:42:48 -0700

    added background image, working on login routing

---
+++ src/components/LoginSignup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function LoginSignup(props) {
+  return (
+    <div>
+      <h1>
+        Login / Sign Up
+      </h1>
+    </div>
+  )
+}
+
+export default LoginSignup;
