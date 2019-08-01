957481deeb467500aa4a61133167601ae22f5371
Dean Ohashi <dnohashi@Deans-MacBook-Pro-2.local>
2019-08-01 12:13:24 -0700

    changed message controller to update messages correctly

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -2,6 +2,7 @@ const Message = require('../models/messageModel.js');
 
 module.exports = messageController = {
   getMessengerList: (req, res, next) => {
+    console.log('retrieving messages');
     Message.find({
       partyOne: req.body.partyOne,
       // partyTwo: req.body.partyTwo
@@ -26,14 +27,16 @@ module.exports = messageController = {
     Message.findOneAndUpdate({ 
       partyOne: req.body.partyOne, 
       partyTwo: req.body.partyTwo 
-    }, { $set: {...req.body} }, { new: true }, (err, updated) => {
-      if(err) return next(err);
+    }, { $push: {messages: req.body.messages} }, { new: true }, (err, updated) => {
+      if(err) return next('err:', err);
 
       Message.findOneAndUpdate({ 
         partyOne: req.body.partyTwo, 
         partyTwo: req.body.partyOne 
-      }, { $set: {...req.body} }, { new: true }, (err, updated) => {
-        if(err) return next(err);
+      }, { $push: {
+        messages: req.body.messages
+      } }, { new: true }, (err, updated) => {
+        if(err) return next('second err: ', err);
         return next();
       });
     });
